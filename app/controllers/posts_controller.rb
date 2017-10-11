42b1a5e20c44905e2d77c0006b76eb837d61816c
Otexas <marcellus.spears@gmail.com>
2017-10-11 18:59:49 -0500

    Checkpoint 27 PostsAndUsers: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+# #12
+  before_action :require_sign_in, except: :show
   def show
 # #19
     @post = Post.find(params[:id])
@@ -11,12 +12,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-# #35
-    @post.topic = @topic
+    @post = @topic.posts.build(post_params)
+# #14
+    @post.user = current_user
 
     if @post.save
       flash[:notice] = "Post was saved."
@@ -34,8 +33,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated."
@@ -59,4 +57,11 @@ class PostsController < ApplicationController
       render :show
     end
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:titile, :body)
+  end
+
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,7 +5,6 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
-    @sponsored_post = SponsoredPost.find(params[:id])
   end
 
   def new
@@ -13,11 +12,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new
-    @sponsored_post = SponsoredPost.find(params[:id])
-    @topic.name = params[:topic][:name]
-    @topic.description = params[:topic][:description]
-    @topic.public = params[:topic][:public]
+    @topic = Topic.new(topic_params)
 
     if @topic.save
       redirect_to @topic, notice: "Topic was saved successfully."
@@ -34,9 +29,7 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
 
-    @topic.name = params[:topic][:name]
-    @topic.description = params[:topic][:description]
-    @topic.public = params[:topic][:public]
+    @topic.assign_attributes(topic_params)
 
     if @topic.save
       flash[:notice] = "Topic was updated."
@@ -57,5 +50,11 @@ class TopicsController < ApplicationController
       flash.now[:alert] = "There was an error deleting the topic."
       render :show
     end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
   end
 end
