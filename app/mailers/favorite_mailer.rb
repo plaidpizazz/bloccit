131f530748605a728c459062820df195aedd9d50
Otexas <marcellus.spears@gmail.com>
2017-10-23 07:57:52 -0500

    Assignment 31 completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -3,7 +3,6 @@ class FavoriteMailer < ApplicationMailer
 
   def new_comment(user, post, comment)
 
-# #18
     headers["Message-ID"] = "<comments/#{comment.id}@mysterious-dawn-32789>"
     headers["In-Reply_To"] = "<post/#{post.id}@mysterious-dawn-32789>"
     headers["References"] = "<post/#{post.id}@mysterious-dawn-32789>"
@@ -12,7 +11,16 @@ class FavoriteMailer < ApplicationMailer
     @post = post
     @comment = comment
 
-# #19
     mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@mysterious-dawn-32789>"
+    headers["In-Reply_To"] = "<post/#{post.id}@mysterious-dawn-32789>"
+    headers["References"] = "<post/#{post.id}@mysterious-dawn-32789>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You're following, #{post.title}!")
   end
 end
